0753db178f5d35ef9a74648ad4db96f7d8067c2e
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-04-26 15:37:17 +0000

    js2 video

--- 02_objects/objects.js
+++ 02_objects/objects.js
@@ -1,5 +1,36 @@
-// singleton
+//1. singleton
 // object.create
+
+//const tinderUser = new Object()//singleton object
+
+const tinderUser ={}//non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedin = false
+
+const regularUser = {
+     email: "samy@buy.com",
+     fullname: {
+          firstname:"sammy",
+          lastname:"nagrare"
+     }
+}
+
+
+//merging of objects
+
+const obj1 ={1:"A",2:"B"}
+const obj2 ={3:"A",4:"B"}
+
+//const obj3= Object.assign({}, obj1,obj2) or
+//spread operator to combine or merge two objects
+const obj3 ={...obj1,...obj2}
+
+
+
+
+
 
 //2.object literals-method
 
