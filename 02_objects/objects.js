49ef9604e0958dc8dcd21df8b3f41fd2f7a01f70
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-04-26 15:09:20 +0000

    basic object

---
+++ 02_objects/objects.js
@@ -0,0 +1,11 @@
+// singleton
+// object.create
+
+//2.object literals-method
+
+const suser = {
+     name: "sathya",
+     age:32,
+     email:"sathyaprakash777.sp@gmail.com",
+     isLoggedin:false
+}
