6fbe230cf7fb405088da9a52daf39d37b17e2e73
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-04-27 02:11:25 +0000

    this function

---
+++ 03_functions/thisf.js
@@ -0,0 +1,11 @@
+const user ={
+    username: "sathya",
+    price:999,
+
+    welcomeMessage : function(){
+        console.log(`${this.username}, welcome to website`)
+    }
+}
+
+
+user.welcomeMessage()
